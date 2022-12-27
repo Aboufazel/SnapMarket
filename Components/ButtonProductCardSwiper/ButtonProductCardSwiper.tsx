b53a6ceb5b1bd55906e05f7f33385f40390773f0
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2022-12-27 16:00:50 +0330

    refactor style mobile storeSelectoreCard done

--- Components/ButtonProductCardSwiper/ButtonProductCardSwiper.tsx
+++ Components/ButtonProductCardSwiper/ButtonProductCardSwiper.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
-import  Grid from '@mui/material/Grid'
+import Grid from '@mui/material/Grid'
 import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+
 type Props = {
-  title:string
-  icon:any,
-  bgcolor:string
+  title: string
+  icon: any,
+  bgcolor: string
 }
 
-const ButtonProductCardSwiper = ({title ,icon,bgcolor}: Props) => {
+const ButtonProductCardSwiper = ({ title, icon, bgcolor }: Props) => {
   return (
     <Grid>
-        <Button variant='outlined' sx={{color:bgcolor,cursor:'pointer'}}>{title} {icon}</Button>
+      <Button variant='outlined' sx={{ color: bgcolor, cursor: 'pointer',px:'10px'}}>
+        <Typography display={'flex'}>{title}{icon}</Typography>
+      </Button>
     </Grid>
   )
 }
--- Components/MobileStoreSelectore/MobileStoreSelectoreCard/CardStore.tsx
+++ Components/MobileStoreSelectore/MobileStoreSelectoreCard/CardStore.tsx
@@ -1,23 +1,26 @@
 import React from 'react'
-import { Grid, Typography } from '@mui/material'
+import { Grid, Typography, Button } from '@mui/material'
 import ButtonProductCardSwiper from '../../ButtonProductCardSwiper/ButtonProductCardSwiper'
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 type Props = {
     title: string,
     dis: string,
-    top:string,
-    bgcolor:string,
+    top: string,
+    bgcolor: string,
 
 }
 
-const CardStore = ({ title, dis ,top,bgcolor}: Props) => {
+const CardStore = ({ title, dis, top, bgcolor }: Props) => {
     return (
-        <Grid zIndex={-1000} position={'absolute'} top={top} right={'0px'} left={'0px'} mt={'-14rem'} height={130} bgcolor={'common.white'} borderRadius={'0.5rem'} boxShadow={1} display={'flex'} justifyContent={'space-between'} p={'6.6rem 1rem 1.6rem'}>
-            <Grid display={'flex'} gap={2} flexDirection={'column'}>
-                <Typography fontSize={'1rem'}>{title}</Typography>
-                <Typography>{dis}</Typography>
+        <Grid zIndex={-1000} position={'absolute'} top={top} right={'0px'} left={'0px'} mt={'-14rem'} height={125} bgcolor={'common.white'} borderRadius={'0.5rem'} boxShadow={1} display={'flex'} justifyContent={'space-between'} p={'6.6rem 1rem 1.6rem'}>
+            <Grid display={'flex'} gap={1} flexDirection={'column'}>
+                <Typography fontSize={'1.2rem'}>{title}</Typography>
+                <Typography fontSize={'1.2rem'} color={'rgb(26, 28, 35)'}>{dis}</Typography>
             </Grid>
-            <ButtonProductCardSwiper  title={'شروع خرید '}icon={<ArrowBackIosNewIcon/>}bgcolor={bgcolor} />
+            <Grid width={'35%'}  display={'flex'} justifyContent={'flex-end'}>
+                <ButtonProductCardSwiper  title={'شروع خرید '}icon={<ArrowBackIosNewIcon/>}bgcolor={bgcolor} />
+               
+            </Grid>
         </Grid>
     )
 }
