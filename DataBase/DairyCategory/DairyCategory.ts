b9f9c14c6d7a81efcf4def5382ea81d962623d85
Aboufazel <a.ab2521@gmail.com>
2022-12-27 09:58:40 +0330

    Feat : Dairy branch create done

---
+++ DataBase/DairyCategory/DairyCategory.ts
@@ -0,0 +1,8 @@
+import {rejects} from "assert";
+import {Simulate} from "react-dom/test-utils";
+import error = Simulate.error;
+import {any, string} from "prop-types";
+
+
+
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,14 @@ import Grid from "@mui/material/Grid";
 import CssBaseLine from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import Theme from "../Theme/Theme";
-import MobileStoreSelectore from "../Components/MobileStoreSelectore/MobileStoreSelectore";
-
+import DekstopHeader from "../Components/Header/DekstopHeader";
 
 export default function Home() {
-  return (
+    return (
     <ThemeProvider theme={Theme}>
       <Grid>
         <CssBaseLine />
-          <MobileStoreSelectore/>
+          <DekstopHeader/>
       </Grid>
     </ThemeProvider>
   );
